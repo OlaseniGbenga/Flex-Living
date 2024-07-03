8772446d3e190badc4b9bae5b388e8d5ab506fe7
Olaseni Gbenga <gj.olaseni@gmail.com>
2024-07-03 12:06:20 +0100

    Completed backdrop for mobile header

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ function toggleMenu() {
   hamburgerMenuId.classList.toggle('mobileNavDisplay');
   cancelMenuId.classList.toggle('mobileNavDisplay');
   mobileNavId.classList.toggle('open');
+  mobileNavBackDrop.classList.toggle('openBackDrop')
 }
 
 hamburgerMenuId.addEventListener('click', toggleMenu);
