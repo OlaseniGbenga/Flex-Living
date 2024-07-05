ef88cb000a511ddcdb5b1db39b2d1c6b89733293
Olaseni Gbenga <gj.olaseni@gmail.com>
2024-07-05 11:09:09 -0700

    Making header reuseable

---
+++ buy/app.js
@@ -0,0 +1,67 @@
+
+
+class Header extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+        <header>
+      <section>
+        <img
+          class="logo"
+          src="./media/images/logo.png"
+          alt="Flex living logo"
+        />
+        <nav id="destop-nav">
+          <ul>
+            <li>Home</li>
+            <li>LandLords</li>
+            <li>Blog</li>
+            <li>Contacts</li>
+          </ul>
+        </nav>
+
+        <img
+          id="hamburgerMenuId"
+          class="hamburgerMenu"
+          src="./media/svg/hamburgerMenu.svg"
+          alt="hamburger menu"
+        />
+        <img
+          id="cancelMenuId"
+          class="cancelMenu mobileNavDisplay"
+          src="./media/svg/cancel.svg"
+          alt="Close menu"
+        />
+      </section>
+      <nav id="mobileNavId" class="mobileNav">
+        <ul>
+          <li>Home</li>
+          <li>LandLords</li>
+          <li>Blog</li>
+          <li>Contacts</li>
+        </ul>
+      </nav>
+      <div id="mobileNavBackDrop" class="mobileNavBackDrop"></div>
+    </header>`;
+
+
+    function toggleMenu() {
+        hamburgerMenuId.classList.toggle('mobileNavDisplay');
+        cancelMenuId.classList.toggle('mobileNavDisplay');
+        mobileNavId.classList.toggle('open');
+        mobileNavBackDrop.classList.toggle('openBackDrop')
+      }
+      
+      hamburgerMenuId.addEventListener('click', toggleMenu);
+      cancelMenuId.addEventListener('click', toggleMenu);
+  }
+}
+
+//Footer
+
+class Footer extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = ``;
+  }
+}
+
+customElements.define("main-header", Header);
