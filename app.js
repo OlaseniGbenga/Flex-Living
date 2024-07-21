b0b59ff509a209430100f97e82af88327b3d4c3f
Olaseni Gbenga <gj.olaseni@gmail.com>
2024-07-21 04:49:39 -0700

    Completed carousel ui

--- app.js
+++ app.js
@@ -13,3 +13,37 @@ import {Footer} from "./components/Footer/Footer.js"
 
 customElements.define("main-header", Header);
 customElements.define("main-footer", Footer);
+
+
+const carouselTrack = document.querySelector(".carousel-track");
+const carouselItems = document.querySelectorAll(".carousel");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+
+const numItems = carouselItems.length;
+
+
+let currentIndex = 0;
+
+function moveCarousel(direction) {
+  if (direction === "next" && currentIndex < (numItems-1)) {
+    currentIndex++;
+  } else if (currentIndex >0 && direction === "prev") {
+    currentIndex--;
+  }
+
+  // Calculate the offset based on currentIndex and itemWidth
+  if (currentIndex <= numItems) {
+
+   const itemWidth = carouselItems[0].clientWidth;
+    const offset = currentIndex * (itemWidth+20) * -1;
+    carouselTrack.style.transition = "transform 0.5s ease";
+    carouselTrack.style.transform = `translateX(${offset}px)`;
+  }
+}
+
+prevBtn.addEventListener("click", () => moveCarousel("prev"));
+nextBtn.addEventListener("click", () => moveCarousel("next"));
+
+
+
